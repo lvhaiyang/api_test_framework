2948e13fe3d9e26a13ba8728a3e9911e213ddb61
lv.haiyang <lv.haiyang@xiaoyi.com>
2018-03-16 13:12:35 +0800

    yi_frame

--- test_framework/lib/encrypt.py
+++ test_framework/lib/encrypt.py
@@ -8,11 +8,5 @@ def sha256_base64(mag, key):
     return base64.b64encode(signature)
 
 
-if __name__ == '__main__':
-    password_key = b'KXLiUdAsO81ycDyEJAeETC$KklXdz3AC'
-    # print(password_key)
-    # print(type(password_key))
-    # print(str(password_key, encoding='utf-8'))
-    msg = '1qaz!QAZ'
-    print(sha256_base64(msg, password_key))
+
 
